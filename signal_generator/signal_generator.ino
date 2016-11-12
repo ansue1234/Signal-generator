e0a8601565e3bae8f501545476671a87d38a836a
Andrew Sue <Andrew Sue>
2016-11-12 12:04:30 -0800

    second

--- signal_generator/signal_generator.ino
+++ signal_generator/signal_generator.ino
@@ -1,48 +1,62 @@
+int i ;
+int f = 400;
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,2);
-void setup() {
-  pinMode(9,OUTPUT);
-  pinMode(8,INPUT);
-  pinMode(7,INPUT);
-  pinMode(1,INPUT);
-  pinMode(13,INPUT);
-  pinMode(10,INPUT);
-  Serial.begin(9600);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+boolean mode;
+void setup(){
   lcd.begin(16, 2);
-  lcd.clear();
+  pinMode(9,OUTPUT);
+  pinMode(13,INPUT);
+  pinMode(6,INPUT);
+  pinMode(8,INPUT);
+  Serial.begin(9600);
 }
 
 
-void loop() {
-lcd.print("HI"); 
-  if(digitalRead(10)==HIGH){
-   for(int i = 140; i<3600; i+=100){
-     tone(9,i);
-     Serial.println(i);
-     lcd.setCursor(0,1);
-     lcd.print(i);
-     delay(1000);
-     if(digitalRead(10)==LOW){
-      noTone(9);
-    }
-
-  }
-  
+void loop(){
+ 
   if(digitalRead(13)==HIGH){
-    int f = 140;
-   // manual();
-    Serial.println("hi");
-    lcd.setCursor(0,1);
-    lcd.print(f);
-    
+   mode=true;
+  }else{
+    mode=false;
   }
-
+  if(mode==true){
+   steps();
+   Serial.println("true");
+  }else if(mode==false){
+    mode2();
+  }
 }
+
+
+void steps(){
+  for(i=400;i<3000;i+=100){
+      tone(9,i);
+      lcd.setCursor(0,1);
+      lcd.print(i);
+      lcd.print("hz");
+      delay(500);
+      lcd.clear();
+    }
 }
 
-
-
-    
-
-
-
+void mode2(){
+  //tone(9,f);
+  lcd.setCursor(0,1);
+  lcd.print(f);  
+  lcd.print("hz");
+  
+  if(digitalRead(6)==HIGH){
+//    f+=100;
+//    delay(200);
+    lcd.clear();
+    lcd.print("LOW");
+  }
+  if(digitalRead(8)==HIGH){
+    //f-=100;
+    //delay(200);
+    lcd.clear();
+    lcd.print("HIGH");
+     
+  }
+}
