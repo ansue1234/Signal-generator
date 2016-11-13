b353d3dc054f0f3efab6117176aea0e051fcf607
Andrew Sue <Andrew Sue>
2016-11-12 21:09:52 -0800

    finished

--- signal_generator/signal_generator.ino
+++ signal_generator/signal_generator.ino
@@ -3,11 +3,14 @@ int f = 400;
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 boolean mode;
+int val;
+int hert;
+
 void setup(){
   lcd.begin(16, 2);
   pinMode(9,OUTPUT);
   pinMode(13,INPUT);
-  pinMode(6,INPUT);
+  pinMode(7,INPUT);
   pinMode(8,INPUT);
   Serial.begin(9600);
 }
@@ -22,41 +25,37 @@ void loop(){
   }
   if(mode==true){
    steps();
-   Serial.println("true");
   }else if(mode==false){
     mode2();
   }
 }
 
-
 void steps(){
-  for(i=400;i<3000;i+=100){
+  for(i=100;i<3100;i+=100){
+    if(digitalRead(13)==LOW){
+      break;
+      mode2();
+      Serial.print(true);
+      
+    }
       tone(9,i);
       lcd.setCursor(0,1);
       lcd.print(i);
       lcd.print("hz");
-      delay(500);
+      delay(1000);
       lcd.clear();
     }
 }
 
 void mode2(){
-  //tone(9,f);
-  lcd.setCursor(0,1);
-  lcd.print(f);  
-  lcd.print("hz");
-  
-  if(digitalRead(6)==HIGH){
-//    f+=100;
-//    delay(200);
-    lcd.clear();
-    lcd.print("LOW");
-  }
-  if(digitalRead(8)==HIGH){
-    //f-=100;
-    //delay(200);
-    lcd.clear();
-    lcd.print("HIGH");
-     
-  }
+ val = analogRead(1);
+ hert = map(val,0,1023,100,3000);
+ tone(9,hert);
+ lcd.setCursor(0,1);
+ lcd.print(hert);
+ lcd.print("hz");
+ delay(50);
+ lcd.clear();  
 }
+
+
